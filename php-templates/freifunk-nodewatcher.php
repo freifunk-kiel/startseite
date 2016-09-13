9123f95c5101a0deac91311896d8be609b91b6f9
rubo77 <github@r.z11.de>
2016-09-13 14:26:49 +0200

    Nodealarm Formular ergänzt

--- php-templates/freifunk-nodewatcher.php
+++ php-templates/freifunk-nodewatcher.php
@@ -5,7 +5,7 @@
  */
 
 $absender_seite="http://freifunk.in-kiel.de";
-$empfaenger = "mailingliste@example.freifunk.net";
+$empfaenger = "nodewatcher-mailingliste@example.freifunk.net";
 
 $absendername = preg_replace("/[^a-zA-Z \-.,_äöüÄÖÜß\(\)]/","",trim($_REQUEST["name"]));
 $absendername = preg_replace("/,/","\,",$absendername);
@@ -25,8 +25,8 @@ if(empty($absendermail)) {
   header("Location: $absender_seite/fehler.html?message=".rawurlencode("Fehler beim Versenden deiner Email! Deine Email ist leer oder enthält ungültige Zeichen!"));
   exit;
 }
-$betreff = "[Kontaktformular Freifunk] $absendername - $absendermail";
-$text = utf8_decode(trim(strip_tags($_REQUEST["body"])));
+$betreff = "[FFKI NODEWATCHER] $absendername - $absendermail"; 
+$text = "Bitte folgende Knoten in den Nodewatcher aufnehmen:\n\n".utf8_decode(trim(strip_tags($_REQUEST["body"])));
 if(mail($empfaenger, $betreff, $text, "From: \"".utf8_encode($absendername)."\" <$absendermail>")){
         header("Location: $absender_seite/bestaetigung.html?message=".rawurlencode("Deine Email wurde erfolgreich versandt."));
 } else {
