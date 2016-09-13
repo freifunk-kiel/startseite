7428b61f126ea59d2bfd69d5d2120f8721a72351
rubo77 <github@r.z11.de>
2016-09-13 16:10:23 +0200

    Knotenalarm Beispielseite in PHP

--- php-templates/freifunk-nodewatcher.php
+++ php-templates/freifunk-nodewatcher.php
@@ -1,13 +1,37 @@
 <?php
 /**
- * Dies ist ein Beispiel für eine PHP Seite, die die Formulardaten empfängt und versendet
+ * dies ist ein Beispiel für eine PHP Seite, die die Formulardaten empfängt und versendet
  * danach wird zurück an die Bestätigungsseite geleitet
  */
 
 $absender_seite="http://freifunk.in-kiel.de";
-$empfaenger = "nodewatcher-mailingliste@example.freifunk.net";
+$nodewatcher_mail = "knotenalarm@lists.freifunk.in-kiel.de";
+$nodewatcher_url='http://freifunk.discovibration.de/freifunk-nodewatcher.php';
+$community_TLD="FFKI";
+$nodewatcher_name="KNOTENALARM";
 
-$absendername = preg_replace("/[^a-zA-Z \-.,_äöüÄÖÜß\(\)]/","",trim($_REQUEST["name"]));
+
+if (!empty($_REQUEST["confirm-email"]) and trim($_REQUEST["confirm-email"])!=""){
+	$confirmmail=$absendermail=preg_replace("/[^a-zA-Z0-9 \-._@]/","",trim($_REQUEST["confirm-email"]));
+}
+if (!empty($_REQUEST["deactivate"]) and trim($_REQUEST["deactivate"])!=""){
+	$deactivatemail=$absendermail=preg_replace("/[^a-zA-Z0-9 \-._@]/","",trim($_REQUEST["deactivate"]));
+}
+if(!empty($confirmmail)){
+	$mess="Du wurdest erfolgreich eingetragen.";
+	mail($confirmmail, '['.$community_TLD.'] erfolgreich eingetragen',$mess, "From: ".$nodewatcher_mail);
+	mail($nodewatcher_mail, '['.$community_TLD.' '.$nodewatcher_name.'] confirmation',$confirmmail, "From: ".$nodewatcher_mail);
+	header("Location: $absender_seite/bestaetigung.html?message=".rawurlencode($mess));
+	exit;
+}
+if(!empty($deactivatemail)){
+	$mess="Du wurdest erfolgreich abgemeldet.";
+	mail($deactivatemail, '['.$community_TLD.'] erfolgreich abgemeldet',$mess, "From: ".$nodewatcher_mail);
+	mail($nodewatcher_mail, '['.$community_TLD.' '.$nodewatcher_name.'] abgemeldet',$deactivatemail, "From: ".$nodewatcher_mail);
+	header("Location: $absender_seite/bestaetigung.html?message=".rawurlencode($mess));
+	exit;
+}
+$absendername = preg_replace("/[^a-zA-Z0.9 \-.,_äöüÄÖÜß\(\)]/","",trim($_REQUEST["name"]));
 $absendername = preg_replace("/,/","\,",$absendername);
 $absendername = preg_replace("/ä/","ae",$absendername);
 $absendername = preg_replace("/ö/","oe",$absendername);
@@ -17,18 +41,41 @@ $absendername = preg_replace("/Ö/","oe",$absendername);
 $absendername = preg_replace("/Ü/","ue",$absendername);
 $absendername = preg_replace("/ß/","ss",$absendername);
 if(empty($absendername)){
-  header("Location: $absender_seite/fehler.html?message=".rawurlencode("Fehler beim Versenden deiner Email! Der Name ist leer oder enthält ungültige Zeichen!"));
+  header("Location: $absender_seite/fehler.html?message=".rawurlencode("Fehler beim Versenden deiner Email! der Name ist leer oder enthält ungültige Zeichen!"));
   exit;
 }
-$absendermail = preg_replace("/[^a-zA-Z \-._@]/","",trim($_REQUEST["from"]));
+$absendermail = preg_replace("/[^a-zA-Z0-9 \-._@]/","",trim($_REQUEST["from"]));
 if(empty($absendermail)) {
   header("Location: $absender_seite/fehler.html?message=".rawurlencode("Fehler beim Versenden deiner Email! Deine Email ist leer oder enthält ungültige Zeichen!"));
   exit;
 }
-$betreff = "[FFKI NODEWATCHER] $absendername - $absendermail"; 
-$text = "Bitte folgende Knoten in den Nodewatcher aufnehmen:\n\n".utf8_decode(trim(strip_tags($_REQUEST["body"])));
-if(mail($empfaenger, $betreff, $text, "From: \"".utf8_encode($absendername)."\" <$absendermail>")){
-        header("Location: $absender_seite/bestaetigung.html?message=".rawurlencode("Deine Email wurde erfolgreich versandt."));
+$betreff = '['.$community_TLD.' '.$nodewatcher_name.'] '.$absendername." - ".$absendermail;
+$body=trim(strip_tags($_REQUEST["body"]));
+$text = "Bitte folgende Knoten in den Nodewatcher aufnehmen:\n\n".utf8_decode($body);
+if(mail($nodewatcher_mail, $betreff, $text, "From: \"".utf8_encode($absendername)."\" <$absendermail>")){
+	#send email confirmation to sender:
+	$confirmtext='Hallo '.$absendername.',
+
+für einen deiner Knoten wurde der automatisierte Versand von Status-E-Mails aktiviert. Um sicherzustellen, dass du wirklich der richtige Empfänger für diese E-Mails bist, bitten wir dich, deine E-Mail-Adresse durch einen Klick auf folgenden Bestätiguns-Link unten zu bestätigen:
+
+'.$nodewatcher_url.'?confirm-email='.$absendermail.'
+
+Erst danach wird der Versand von Status-E-Mails wirklich stattfinden.
+
+Knoten:
+'.$body.'
+Empfänger 	'.$absendermail.'
+
+Bei Fragen wende dich gerne an freifunk@lists.in-kiel.de
+
+Viele Grüße
+dein Freifunk Kiel
+
+Möchtest du keine Status-E-Mails zu diesem Knoten mehr erhalten, so kannst du den Versand jederzeit deaktivieren:
+'.$nodewatcher_url.'?deactivate='.$absendermail.'
+';
+	mail($absendermail, '['.$community_TLD.'] '.ucfirst($nodewatcher_name).' bestätigen', utf8_decode($confirmtext), "From: ".$nodewatcher_mail);
+	header("Location: $absender_seite/bestaetigung.html?message=".rawurlencode("Du wurdest eingetragen. Bitte schaue in deinem Postfach nach der Bestätigungs-Email. Erst, wenn du diese beantwortet hast, wirst du benachrichtigt."));
 } else {
-        header("Location: $absender_seite/fehler.html?message=".rawurlencode("Fehler beim Versenden deiner Email!"));
+	header("Location: $absender_seite/fehler.html?message=".rawurlencode("Fehler beim Versenden deiner Email!"));
 }
