6c14da21699cf7191d754941eae9bb1e441f0611
root <root@eclabs.de>
2016-12-02 03:14:32 +0100

    numnodes.sh fuer hopglass angepasst;keine Dateien mehr lokal ablegen.

--- numnodes.sh
+++ numnodes.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # reads all online nodes and creates an HTML page from the template numnodes.template
-JSON=/opt/ffmap-backend-neu/json/nodes.json
-#wget http://map.freifunk.in-kiel.de/json/nodes.json -qO $JSON
+JSON=$(wget http://127.0.0.1:4000/ffapi.json -qO -)2> /dev/null
 
 #echo -n "online nodes: "
-num="$(cat $JSON | jq '.nodes[] | select(.flags.gateway==false) | select(.flags.online==true)' | grep -c online)" 2> /dev/null
+num="$(echo $JSON | jq '.state.nodes')" 2> /dev/null
 
 if test "$num" -ge 1; then
 
@@ -13,13 +12,4 @@ if test "$num" -ge 1; then
 
 	sed 's/<NUM_NODES>/'"$num"'/g' numnodes.template > _site/numnodes.html
 
-else
-
-	cp "$JSON" "$JSON.defekt"
-	BAK="$(find /opt/ffmap-backend-neu/json -name 'nodes.json.bak*' -printf '%T+\t%p\n' | sort -r | head -n1 | cut -f2)"
-	cp "$BAK" "$JSON"
-
 fi	
-
-#echo -n "offline nodes: "
-#cat $JSON |jq '.nodes[] | select(.flags.gateway==false) | select(.flags.online==false)'|grep online|wc -l
