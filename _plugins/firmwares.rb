bb47a0810e032064281a41a03511c94d5fd239b1
Tobias Schramm <tobleminer@gmail.com>
2019-07-09 00:15:23 +0200

    Bump up version to stable release

--- _plugins/firmwares.rb
+++ _plugins/firmwares.rb
@@ -12,7 +12,7 @@ require 'pp'
 COMMUNITY_TLD = 'ffki'
 FIRMWARE_PREFIX = 'gluon-' + COMMUNITY_TLD
 #FIRMWARE_VERSION = '2018.1~exp-215'
-FIRMWARE_VERSION = '2018.1.4-612'
+FIRMWARE_VERSION = '2018.2.2-747'
 
 FIRMWARE_REGEX = Regexp.new('^' + FIRMWARE_PREFIX + '-' + FIRMWARE_VERSION + '-')
 #FIRMWARE_BASE = site.config['firmware']['base']
